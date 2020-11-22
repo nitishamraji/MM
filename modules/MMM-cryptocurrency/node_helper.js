64b00b12a55a97e64b4d0552ed64b41b693c095a
nitish amraji <nitish@nitishs-MacBook-Pro.local>
2020-11-22 14:29:19 -0600

    new

--- modules/MMM-cryptocurrency/node_helper.js
+++ modules/MMM-cryptocurrency/node_helper.js
@@ -14,10 +14,10 @@ module.exports = NodeHelper.create({
 
   getTickers: function (url) {
     var self = this;
-    console.log("getting crypto with url: " url);
+    console.log("getting crypto with url: " + url);
     request({url: url, method: 'GET'}, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        console.log("crypto with url: " JSON.parse(body));
+        console.log("crypto with url: " + JSON.parse(body));
         self.sendSocketNotification('got_result', JSON.parse(body))
       }
     })
