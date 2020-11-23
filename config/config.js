911b09b2d5ddc9ca68e1ce7fb3d2f2dd32909e62
nitish amraji <nitish@nitishs-MacBook-Pro.local>
2020-11-22 18:19:06 -0600

    new

--- config/config.js
+++ config/config.js
@@ -143,19 +143,19 @@ var config = {
 				playSound: false
 			}
 		},
-		// {
-		// 	module: "MMM-cryptocurrency",
-		// 	position: "bottom_left",
-		// 	config: {
-		// 		//apikey: '1234-5678-9009-8765-4321',
-		// 		apikey: 'c37b5d0b-203c-4230-a242-84109c7bbf96',
-		// 		currency: ['bitcoin', 'ethereum', 'litecoin', 'cardano'],
-		// 		conversion: 'USD',
-		// 		headers: ['change24h', 'change1h', 'change7d'],
-		// 		displayType: 'logoWithChanges',
-		// 		showGraphs: true
-		// 	}
-		// },
+		{
+			module: "MMM-cryptocurrency",
+			position: "bottom_left",
+			config: {
+				//apikey: '1234-5678-9009-8765-4321',
+				apikey: 'c37b5d0b-203c-4230-a242-84109c7bbf96',
+				currency: ['bitcoin', 'ethereum'],
+				conversion: 'USD',
+				headers: ['change24h', 'change1h', 'change7d'],
+				displayType: 'logoWithChanges',
+				showGraphs: true
+			}
+		},
 		{
     		module: "MMM-Stock",
     		position: "bottom_right",
@@ -163,13 +163,13 @@ var config = {
     			companies: ["MSFT", "GOOG", "ORCL", "FB", "AAPL"]
     		}
 		},
-		{
-    		module: "MMM-Stock-crypto", //Z2AUZ6MDYV2790ER
-    		position: "bottom_left",
-    		config: {
-    			companies: ["SPY", "QQQ", "IWM", "XLF", "GLD"]
-    		}
-		}
+		// {
+  //   		module: "MMM-Stock-crypto", //Z2AUZ6MDYV2790ER
+  //   		position: "bottom_left",
+  //   		config: {
+  //   			companies: ["SPY", "QQQ", "IWM", "XLF", "GLD"]
+  //   		}
+		// }
 	]
 };
 
